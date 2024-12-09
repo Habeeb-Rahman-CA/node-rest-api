454cd00586f75fce0c534961c869f9d763baa4db
HabeebR07 <habeebrahmanca07@gmail.com>
2024-12-09 16:49:28 +0530

    added put and delete api, final commit of nodejs rest api project

--- methods/delete-request.js
+++ methods/delete-request.js
@@ -1,1 +1,20 @@
-module.exports = (req, res) => {}
+const writeToFile = require("../utils/write-to-file")
+module.exports = (req, res) => {
+    let baseUrl = req.url.substring(0, req.url.lastIndexOf("/") + 1)
+    let id = req.url.split("/")[3]
+    if (baseUrl === "/api/movies/") {
+        const index = req.movies.findIndex((movie) => {
+            return movie.id === id
+        })
+        if (index === -1) {
+            res.statusCode = 404
+            res.write(JSON.stringify({ title: "Not Found", message: "Movie not found" }))
+            res.end()
+        } else {
+            req.movies.splice(index, 1)
+            writeToFile(req.movies)
+            res.writeHead(203, {"Content-Type": "application/json"})
+            res.end(JSON.stringify(req.movies))
+        }
+    }
+}
--- methods/put-request.js
+++ methods/put-request.js
@@ -1,1 +1,31 @@
-module.exports = (req, res) => {}
+const requestBodyparser = require("../utils/body-parser")
+const writeToFile = require("../utils/write-to-file")
+module.exports = async(req, res) => {
+    let baseUrl = req.url.substring(0, req.url.lastIndexOf("/") + 1)
+    let id = req.url.split("/")[3]
+    if (baseUrl === "/api/movies/") {
+        try {
+            let body = await requestBodyparser(req)
+            const index = req.movies.findIndex((movie) => {
+                return movie.id === id
+            })
+            if (index === -1) {
+                res.statusCode = 404
+                res.write(JSON.stringify({ title: "Not Found", message: "Movie not found" }))
+                res.end()
+            } else {
+                req.movies[index] = {id, ...body}
+                writeToFile(req.movies)
+                res.writeHead(200, {"Content-Types": "application/json"})
+                res.end(JSON.stringify(req.movies[index]))
+            }
+        } catch (error) {
+            console.log(error)
+            res.writeHead(404, { "Content-Type": "application/json" })
+            res.end(JSON.stringify({ title: "Validation failed", message: "Route not found" }))
+        }
+    } else {
+        res.writeHead(404, { "Content-Type": "application/json" })
+        res.end(JSON.stringify({ title: "Not Found", message: "Route not found" }))
+    }
+}
